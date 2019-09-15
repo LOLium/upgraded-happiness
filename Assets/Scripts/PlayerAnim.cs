fa892b869d7b95fa58beda96c07f2d783f04d293
deptster <deepantripathi0000@gmail.com>
2019-09-16 01:27:33 +0530

    sucess 2
    + animations
    + multiplayer

---
+++ Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerAnim : NetworkBehaviour
+{
+    public Animator playerAnimation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkForPlayerInput();
+    }
+
+    void checkForPlayerInput()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        if(Mathf.Abs(Input.GetAxis("Vertical"))>0||
+            Mathf.Abs(Input.GetAxis("Horizontal"))>0)
+        {
+            playerAnimation.SetBool("moving", true);
+        }
+        else
+        {
+            playerAnimation.SetBool("moving", false);
+        }
+    }
+}
