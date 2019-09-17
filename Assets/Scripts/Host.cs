52f2e845d1cb7d2bed492c58c466d61b0422e5dd
deptster <deepantripathi0000@gmail.com>
2019-09-17 21:50:44 +0530

    long way to go
    +raise maze terrain by -0.1
    +add kenny assets
    +Host scripting (1/2)
    +door animation fix

---
+++ Assets/Scripts/Host.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Host : MonoBehaviour
+{
+    [SerializeField]
+    uint RoomSize = 2;
+    private string RoomName;
+
+    private NetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+        if(networkManager.matchMaker == null)
+        {
+            networkManager.StartMatchMaker();
+        }
+    }
+
+    public void SetRoomName(string name)
+    {
+        RoomName = name;
+    }
+
+    public void CreateRoom()
+    {
+        if(RoomName != "" && RoomName != null)
+        {
+            Debug.Log("Creating Room: " + RoomName + " with room for " + RoomSize + " players.");
+            networkManager.matchMaker.CreateMatch(RoomName, RoomSize, true, "", "", "", 0, 0, networkManager.OnMatchCreate);
+        }
+    }
+}
