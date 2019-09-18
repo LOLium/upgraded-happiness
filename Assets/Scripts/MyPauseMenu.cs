9e1504d204f94993021bbcdd9a04c57b5bb2120c
deptster <deepantripathi0000@gmail.com>
2019-09-18 22:44:04 +0530

    bleh
    +enclosuer
    etc

---
+++ Assets/Scripts/MyPauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using UnityEngine;
+
+public class MyPauseMenu : MonoBehaviour
+{
+    public static bool IsOn = false;
+
+    private NetworkManager networkManagerForMenu;
+
+    private void Start()
+    {
+        networkManagerForMenu = NetworkManager.singleton;
+    }
+
+    public void LeaveRoom()
+    {
+        MatchInfo matchInfo = networkManagerForMenu.matchInfo;
+        networkManagerForMenu.matchMaker.DropConnection(matchInfo.networkId, matchInfo.nodeId, 0, networkManagerForMenu.OnDropConnection);
+        networkManagerForMenu.StopHost();
+    }
+
+}
